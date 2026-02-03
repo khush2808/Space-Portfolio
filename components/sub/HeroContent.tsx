ac2bf4a4cfd712d0fe2b3354670dba89ddfb982f
Khush Shah <114491725+khush2808@users.noreply.github.com>
2026-02-04 00:44:00 +0530

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -39,7 +39,7 @@ const HeroContent = () => {
           
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              Software Engineer{" "}
+              Software Engineer{"\n "}
             </span>
         </motion.div>
 
